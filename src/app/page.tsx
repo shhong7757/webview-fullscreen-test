5b6e102cfe2aa8c8697f52f2c807ce8f4c1bd1d4
shhong7757 <ghdtkdgur123@gmail.com>
2023-06-29 20:03:19 +0900

    래핑 태그 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,90 @@
 export default function Home() {
   return (
-    <div>
-      <p> {"span > div > figure > div > video 비디오 1"} </p>
-      <span />
-      <span>
-        <div>
-          <figure
-            style={{
-              width: "100%",
-              position: "relative",
-              display: "inline-block",
-              paddingBottom: "56.25%",
-              overflow: "hidden",
-              outline: "none",
-              //
-              boxSizing: "border-box",
-            }}
-          >
-            <div
-              style={{
-                position: "absolute",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                width: "100%",
-                height: "100%",
-                //
-                boxSizing: "border-box",
-              }}
-            >
-              <video
-                width="100%"
-                height="100%"
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  //
-                  pointerEvents: "auto",
-                }}
-                controls
-                src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
-              />
-            </div>
-          </figure>
-        </div>
-      </span>
-      <p> {"span > div > figure > div > video 비디오 2"} </p>
-      <span />
-      <span>
-        <div>
-          <figure>
+    <div
+      dir="auto"
+      data-presentation-mode="false"
+      style={{
+        boxSizing: "border-box",
+      }}
+    >
+      <div
+        contentEditable={false}
+        translate="no"
+        style={{
+          height: "100%",
+          overflowWrap: "break-word",
+          wordBreak: "keep-all",
+          hyphens: "auto",
+          //
+          position: "relative",
+        }}
+      >
+        <p>
+          <p> {"span > div > figure > div > video 비디오 1"} </p>
+          <span />
+          <span>
             <div>
-              <video
+              <figure
                 style={{
-                  maxWidth: "100%",
-                  height: "auto",
+                  width: "100%",
+                  position: "relative",
+                  display: "inline-block",
+                  paddingBottom: "56.25%",
+                  overflow: "hidden",
+                  outline: "none",
+                  //
+                  boxSizing: "border-box",
                 }}
-                controls
-                src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
-              />
+              >
+                <div
+                  style={{
+                    position: "absolute",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "100%",
+                    height: "100%",
+                    //
+                    boxSizing: "border-box",
+                  }}
+                >
+                  <video
+                    width="100%"
+                    height="100%"
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      //
+                      pointerEvents: "auto",
+                    }}
+                    controls
+                    src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+                  />
+                </div>
+              </figure>
             </div>
-          </figure>
-        </div>
-      </span>
+          </span>
+          <p> {"span > div > figure > div > video 비디오 2"} </p>
+          <span />
+          <span>
+            <div>
+              <figure>
+                <div>
+                  <video
+                    style={{
+                      maxWidth: "100%",
+                      height: "auto",
+                    }}
+                    controls
+                    src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+                  />
+                </div>
+              </figure>
+            </div>
+          </span>
+        </p>
+      </div>
     </div>
   );
 }

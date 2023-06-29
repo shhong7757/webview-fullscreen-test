a88226ad3f6944b8fcddce248653bfd4e3994e98
shhong7757 <ghdtkdgur123@gmail.com>
2023-06-29 19:25:20 +0900

    비디오를 감싸는 태그를 구분한 두개의 동영상을 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,35 @@
 export default function Home() {
   return (
-    <div
-      style={{
-        height: "100%",
-        left: 0,
-        position: "absolute",
-        top: 0,
-        width: "100%",
-        zIndex: 0,
-      }}
-    >
-      <div
-        style={{
-          cursor: "pointer",
-          width: 815,
-          height: 456.564,
-        }}
-      >
-        <video
-          controls
-          width="100%"
-          height="100%"
-          src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
-        />
+    <>
+      <p> div > div > video </p>
+      <span />
+      <div>
+        <div>
+          <video
+           style={{
+              maxWidth: '100%',
+              height: 'auto',
+           }}
+            controls
+            src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+          />
+        </div>
       </div>
-    </div>
+      <div />
+      <span />
+      <p> div > figure > video </p>
+      <div>
+        <figure>
+          <video
+            style={{
+              maxWidth: '100%',
+              height: 'auto',
+            }}  
+            controls
+            src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+          />
+        </figure>
+      </div>
+    </>
   );
 }

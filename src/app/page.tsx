c67ced550cd4c5f58d6dd9c1e1455a8aca2aea59
shhong7757 <ghdtkdgur123@gmail.com>
2023-06-29 19:48:33 +0900

    1개 영상 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
           />
         </div>
       </div>
-      <div />
       <span />
       <p> {"div > figure > video"} </p>
       <div>
@@ -30,7 +29,9 @@ export default function Home() {
           />
         </figure>
       </div>
-      <p> {"div > figure > video"} </p>
+      <span />
+      <p> {"span > figure > video"} </p>
+      <span />
       <span>
         <figure>
           <video
@@ -43,6 +44,9 @@ export default function Home() {
           />
         </figure>
       </span>
+      <span />
+      <p> {"span > div > video"} </p>
+      <span />
       <span>
         <div>
           <video
@@ -53,6 +57,40 @@ export default function Home() {
             controls
             src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
           />
+        </div>
+      </span>
+      <span />
+      <p> {"span > div > video"} </p>
+      <span />
+      <span>
+        <div>
+          <video
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+            controls
+            src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+          />
+        </div>
+      </span>
+      <span />
+      <p> {"span > div > figure > div > video"} </p>
+      <span />
+      <span>
+        <div>
+          <figure>
+            <div>
+              <video
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
+                controls
+                src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+              />
+            </div>
+          </figure>
         </div>
       </span>
     </div>

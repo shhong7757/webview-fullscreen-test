32f2733deabecc2b9b78c13d34183d6d6a93ed04
shhong7757 <ghdtkdgur123@gmail.com>
2023-06-29 19:40:57 +0900

    2개 영상 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
       <div>
         <div>
           <video
-           style={{
-              maxWidth: '100%',
-              height: 'auto',
-           }}
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
             controls
             src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
           />
@@ -22,14 +22,39 @@ export default function Home() {
         <figure>
           <video
             style={{
-              maxWidth: '100%',
-              height: 'auto',
-            }}  
+              maxWidth: "100%",
+              height: "auto",
+            }}
             controls
             src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
           />
         </figure>
       </div>
+      <p> {"div > figure > video"} </p>
+      <span>
+        <figure>
+          <video
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+            controls
+            src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+          />
+        </figure>
+      </span>
+      <span>
+        <div>
+          <video
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+            controls
+            src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+          />
+        </div>
+      </span>
     </div>
   );
 }

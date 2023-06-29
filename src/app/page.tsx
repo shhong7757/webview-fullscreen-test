95e6a3c5f447f791cbebe014035d750be4b1d077
shhong7757 <ghdtkdgur123@gmail.com>
2023-06-29 20:18:27 +0900

    비디오 태그 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,123 @@
 export default function Home() {
   return (
-    <div style={{
-      gridArea: "postContent / postContent / postContent / postContent",
-      minWidth: 0,
-      //
-      boxSizing: "border-box",
-    }}>
-      <div>
-        <div
-          style={{
-            minHeight: "21.75rem",
-
-            //
-            boxSizing: "border-box",
-          }}
-        >
+    <div
+      style={{
+        gridArea: "post / post / post / post",
+        display: "grid",
+        gridTemplateRows: "auto auto 1fr",
+      }}
+    >
+      <div
+        style={{
+          gridArea: "postContent / postContent / postContent / postContent",
+          minWidth: 0,
+          //
+          boxSizing: "border-box",
+        }}
+      >
+        <div>
           <div
             style={{
+              minHeight: "21.75rem",
+
+              //
               boxSizing: "border-box",
             }}
           >
             <div
-              dir="auto"
-              data-presentation-mode="false"
               style={{
                 boxSizing: "border-box",
               }}
             >
               <div
-                contentEditable={false}
-                translate="no"
+                dir="auto"
+                data-presentation-mode="false"
                 style={{
-                  height: "100%",
-                  overflowWrap: "break-word",
-                  wordBreak: "keep-all",
-                  hyphens: "auto",
-                  //
-                  position: "relative",
+                  boxSizing: "border-box",
                 }}
               >
-                <p>
-                  <p> {"span > div > figure > div > video 비디오 1"} </p>
-                  <span />
-                  <span>
-                    <div>
-                      <figure
-                        style={{
-                          width: "100%",
-                          position: "relative",
-                          display: "inline-block",
-                          paddingBottom: "56.25%",
-                          overflow: "hidden",
-                          outline: "none",
-                          //
-                          boxSizing: "border-box",
-                        }}
-                      >
-                        <div
+                <div
+                  contentEditable={false}
+                  translate="no"
+                  style={{
+                    height: "100%",
+                    overflowWrap: "break-word",
+                    wordBreak: "keep-all",
+                    hyphens: "auto",
+                    //
+                    position: "relative",
+                  }}
+                >
+                  <p>
+                    <p> {"span > div > figure > div > video 비디오 1"} </p>
+                    <span />
+                    <span>
+                      <div>
+                        <figure
                           style={{
-                            position: "absolute",
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
                             width: "100%",
-                            height: "100%",
+                            position: "relative",
+                            display: "inline-block",
+                            paddingBottom: "56.25%",
+                            overflow: "hidden",
+                            outline: "none",
                             //
                             boxSizing: "border-box",
                           }}
                         >
-                          <video
-                            width="100%"
-                            height="100%"
+                          <div
                             style={{
                               position: "absolute",
-                              top: 0,
-                              left: 0,
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              width: "100%",
+                              height: "100%",
                               //
-                              pointerEvents: "auto",
+                              boxSizing: "border-box",
                             }}
-                            controls
-                            src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
-                          />
-                        </div>
-                      </figure>
-                    </div>
-                  </span>
-                  <p> {"span > div > figure > div > video 비디오 2"} </p>
-                  <span />
-                  <span>
-                    <div>
-                      <figure>
-                        <div>
-                          <video
-                            style={{
-                              maxWidth: "100%",
-                              height: "auto",
-                            }}
-                            controls
-                            src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
-                          />
-                        </div>
-                      </figure>
-                    </div>
-                  </span>
-                </p>
+                          >
+                            <video
+                              width="100%"
+                              height="100%"
+                              style={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                //
+                                pointerEvents: "auto",
+                                //
+                                objectFit: "contain",
+                                overflowClipMargin: "content-box",
+                                overflow: "clip",
+                              }}
+                              controls
+                              src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+                            />
+                          </div>
+                        </figure>
+                      </div>
+                    </span>
+                    <p> {"span > div > figure > div > video 비디오 2"} </p>
+                    <span />
+                    <span>
+                      <div>
+                        <figure>
+                          <div>
+                            <video
+                              style={{
+                                maxWidth: "100%",
+                                height: "auto",
+                              }}
+                              controls
+                              src="https://media.fmkorea.com/files/attach/new3/20230629/486616/4799060287/5918201749/8d7da534ce2661fe44fa3118d6e3e689.mp4"
+                            />
+                          </div>
+                        </figure>
+                      </div>
+                    </span>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
